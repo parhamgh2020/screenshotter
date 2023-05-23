dd45e2d99c0d4f86553d3dba35e008be2e006b8e
amirayat <amirayat20@gmail.com>
2023-05-23 12:40:32 +0330

    set cron job to run

--- screen_shotter.sh
+++ screen_shotter.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+export DISPLAY=:1
+xhost +si:localuser:scrot
+
+weekday=$(date +%w)
+currenttime=$(date +%H%M)
+
+if [ $weekday -gt 0 ] && 
+   [ $weekday -lt 6 ] && 
+   [ $currenttime -gt "0859" ] && 
+   [ $currenttime -lt "1801" ]; then
 cd ~/Pictures
 folder_name=$(date +"%Y-%m-%d")
 mkdir -p $folder_name
 
-while true
-do
-    # Define the file names with the current timestamps
-    image_name="$(date '+%Y-%m-%d %H:%M:%S').jpg"
+# Define the file names with the current timestamps
+image_name="$(date '+%Y-%m-%d %H:%M:%S').jpg"
 
-    cd /tmp
+cd /tmp
 
-    # Take the screenshot of the laptop display using scrot
-    scrot "$image_name" -z
+# Take the screenshot of the laptop display using scrot
+scrot "$image_name" -z
 
-    # Resize image
-    mogrify -resize 1200x800 "$image_name"
+# Resize image
+mogrify -resize 1200x800 "$image_name"
 
-    # reduce quality
-    jpegoptim --size=50k "$image_name"
+# reduce quality
+jpegoptim --size=50k "$image_name"
 
-    # copy to the right folder
-    cp "$image_name" ~/Pictures/$folder_name/
-
-    # Wait for 1 minute before taking the next screenshot
-    sleep 60
-done
+# copy to the right folder
+cp "$image_name" ~/Pictures/$folder_name/
+fi
